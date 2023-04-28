0a87845b31cb02ed925001bc816100d4e4a0ee47
Samson Tush <samo4421@icloud.com>
2023-04-28 10:58:20 +0300

    Fix linter changes

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -3,10 +3,9 @@ import renderer from 'react-test-renderer';
 import Header from '../component/Header';
 
 describe('Header component', () => {
-    test('should render correctly', () => {
-      const component = renderer.create(<Header />);
-      const tree = component.toJSON();
-      expect(tree).toMatchSnapshot();
-    });
+  test('should render correctly', () => {
+    const component = renderer.create(<Header />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
   });
-  
+});
