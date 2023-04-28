8fa57f0abbd9a0357bdf5aad7365b159d6f508bb
Samson Tush <samo4421@icloud.com>
2023-04-28 10:17:56 +0300

    Solve linter errors

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import Header from '../component/Header';
-
-import { configure } from 'enzyme';
+import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import Header from '../component/Header';
 
 configure({ adapter: new Adapter() });
 
